55896aa5cc80489a517e7b18794311765d8e23c8
bu1th4nh <thanh10973@gmail.com>
2018-06-13 07:40:17 +0700

    Added code for problem NPR-SPOJ

--- SPOJ/SPOJ_NPR/main.cpp
+++ SPOJ/SPOJ_NPR/main.cpp
@@ -58,19 +58,69 @@ using namespace std;
 #define FILEOP_DEBUG() {freopen(task".inp", "r", stdin); freopen(task".out", "w", stdout); freopen(task".err", "w", stderr);}
 
 
+const int inf = 1234567890;
+//=====================================
+//Trie
+struct Node
+{
+    int w, min1, min2;
+    Node* child[27];
+    Node* parent;
+};
+Node* newNode()
+{
+    Node* ret = new Node;
+
+    FOR(i, 0, 26) ret->child[i] = NULL;
+    ret->parent = NULL;
+
+    ret->w = -1; //Assume that there's no word ends here.
+    ret->min1 = ret->min2 = inf;
+
+    return ret;
+}
+Node* root = newNode();
+void insert(string st, int id)
+{
+    Node* p = root;
+    for(char c: st)
+    {
+        if(p->min1 == p->min2)
+        {
+            if(p->min1 < inf) p->min2 = id;
+            else    p->min1 = p->min2 = id;
+        }
+
+        int i = c - 'a';
+        if(p->child[i] == NULL) p->child[i] = newNode();
+
+        p->child[i]->parent = p;                        //Assign parent.
+        p = p->child[i];
+    }
+    p->w = id; //A word ends here, determined by id.
+
+    if(p->min1 == p->min2)
+    {
+        if(p->min1 < inf) p->min2 = id;
+        else    p->min1 = p->min2 = id;
+    }
+
+}
+
 //=====================================
 //Typedefs
 typedef long long ll;
 typedef unsigned long long ull;
 typedef pair<int, int> ii;
+typedef vector<string> vs;
 typedef vector<bool> vb;
 typedef vector<int> vi;
 typedef vector<ii> vii;
 typedef vector<vi> vvi;
 typedef vector<vb> vvb;
 typedef vector<vii> vvii;
-
-
+int n;
+vs a;
 
 
 //=====================================
@@ -93,29 +143,95 @@ void FileClose()
 //Enter
 void Enter()
 {
-	//Enter value
+    n = 0;
+    string s;
+    char str[100000];
+
+	while(1)
+    {
+        getline(cin, s);
+        if(s == "") break;
+        a.push_back(s), ++n;
+    }
+    sort(whole(a));
 }
 
 //Check
 
+int find(string st)
+{
+    Node* p = root;
+    reverse(whole(st));
+    for(char c: st)
+    {
+        int i = c - 'a';
+        if(p->child[i] == NULL)
+        {
+            //cerr << "General Case: ";
+            return p->min1;
+        }
+
+        p = p->child[i];
+    }
+
+    if(p->w == -1)
+    {
+
+        //cerr << "CC1: ";
+
+        return p->min1;
+    }
+    else
+    {
+        //cerr << "CC2: ";
+
+        int lkp = p->w;
+        while(p->parent != NULL && p->min1 == p->min2)
+        {
+            p = p->parent;
+            //cerr << " -{" << p->min1 << ", " << p->min2 << "}- ";
+        }
+
+        if(lkp != p->min1) return p->min1;
+        return p->min2;
+    }
+}
 
 
+void Query(string t)
+{
+    int id = find(t) - 1;
+    string res = a[id];
+    cout << res << el;
+}
 
 
 //Process
+void Solve()
+{
+    char str[100000];
+    sort(whole(a));  int ptr = 0;
 
+    //cerr << el;
+    for(string s: a)
+    {
+        //cerr << s << sp << ptr+1 << el;
 
+        reverse(whole(s));
+        insert(s, ++ptr);
+    }
+    cerr << el;
 
-
-//Output
-
-
-
-
+    while(scanf("%s", &str) != EOF)
+    {
+        Query(string(str));
+    }
+}
 
 //Main Procedure
 int main()
 {
-    //Your code goes here
+    Enter();
+    Solve();
     return 0;
 }
