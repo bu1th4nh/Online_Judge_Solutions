11335ceed837a2585a70c0f08805fb2c3fc4c41a
bu1th4nh <thanh10973@gmail.com>
2018-09-23 01:09:35 +0700

    What is SprayTime?

---
+++ Codeforces - SprayTime/1B/main.cpp
@@ -0,0 +1,132 @@
+/*==========================================================================================*\
+**                        _           _ _   _     _  _         _                            **
+**                       | |__  _   _/ | |_| |__ | || |  _ __ | |__                         **
+**                       | '_ \| | | | | __| '_ \| || |_| '_ \| '_ \                        **
+**                       | |_) | |_| | | |_| | | |__   _| | | | | | |                       **
+**                       |_.__/ \__,_|_|\__|_| |_|  |_| |_| |_|_| |_|                       **
+\*==========================================================================================*/
+//Libraries and namespaces
+//#include <bits/stdc++.h>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <vector>
+#include <utility>
+
+#if __cplusplus >= 201103L
+#include <unordered_map>
+#include <unordered_set>
+#include <random>
+#endif // __cplusplus
+
+using namespace std;
+
+
+//=====================================
+//Macroes
+#define sp " "
+#define el "\n"
+#define task "1B"
+#define fi first
+#define se second
+#define pb push_back
+#define maxinp (int)()
+#define siz(x) (int)(x.size())
+#define len(x) (int)(x.length())
+#define whole(x) x.begin(), x.end()
+#define FOR(i, x, y) for(int i=x; i<=y; ++i)
+#define FORl(i, x, y) for(int i=x; i<y; ++i)
+#define FORb(i, x, y) for(int i=x; i>=y; --i)
+#define FORlb(i, x, y) for(int i=x; i>y; --i)
+#define MEMS(x, val) memset(x, val, sizeof(x))
+#define FILEOP() {freopen(task".inp", "r", stdin); /*freopen(task".out", "w", stdout);*/}
+#define FILEOP_DEBUG() {freopen(task".inp", "r", stdin); freopen(task".out", "w", stdout); freopen(task".err", "w", stderr);}
+
+
+//=====================================
+//Typedefs
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vb> vvb;
+typedef vector<vii> vvii;
+typedef vector<string> vs;
+int r, c;
+
+
+//=====================================
+//Functions and procedures
+//Initialization and preparation
+void FileInit()
+{
+    FILEOP()
+}
+void FileDebug()
+{
+    FILEOP_DEBUG()
+}
+void FileClose()
+{
+    fclose(stdin);
+    fclose(stdout);
+}
+
+//Process
+void print(int c)
+{
+    if(!c) return;
+    print((c - 1) / 26);
+    putchar(((c - 1) % 26) + 'A');
+}
+void Solve()
+{
+    char str[202020], t[202020], *p;
+    scanf("%s", &str);
+
+    if(sscanf(str, "R%dC%d", &r, &c) == 2)
+    {
+        print(c);
+        printf("%d\n", r);
+    }
+    else
+    {
+        sscanf(str, "%[A-Z]%d", t, &r);
+        for(p = t, c = 0; *p; p++)
+        {
+            c = c * 26 + int(*p - 'A' + 1);
+        }
+        printf("R%dC%d\n", r, c);
+    }
+}
+
+
+//Main Procedure
+int main()
+{
+    //FileInit();
+    int T; cin >> T;
+    while(T--)
+    {
+        Solve();
+    }
+    return 0;
+}
