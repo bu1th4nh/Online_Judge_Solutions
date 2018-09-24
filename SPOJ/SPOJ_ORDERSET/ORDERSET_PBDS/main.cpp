5707ade1de35fe506ca11c1f7171e37b18a8c7cf
bu1th4nh <thanh10973@gmail.com>
2018-09-24 09:25:04 +0700

    That's what i call: Magic!

---
+++ SPOJ/SPOJ_ORDERSET/ORDERSET_PBDS/main.cpp
@@ -0,0 +1,129 @@
+/*==========================================================================================*\
+**                        _           _ _   _     _  _         _                            **
+**                       | |__  _   _/ | |_| |__ | || |  _ __ | |__                         **
+**                       | '_ \| | | | | __| '_ \| || |_| '_ \| '_ \                        **
+**                       | |_) | |_| | | |_| | | |__   _| | | | | | |                       **
+**                       |_.__/ \__,_|_|\__|_| |_|  |_| |_| |_|_| |_|                       **
+\*==========================================================================================*/
+//Libraries and namespaces
+//#include <bits/stdc++.h>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <vector>
+#include <utility>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#if __cplusplus >= 201103L
+#include <unordered_map>
+#include <unordered_set>
+#include <random>
+#endif // __cplusplus
+
+using namespace __gnu_pbds;
+using namespace std;
+
+
+//=====================================
+//Macroes
+#define sp " "
+#define el "\n"
+#define task "test"
+#define fi first
+#define se second
+#define pb push_back
+#define maxinp (int)()
+#define siz(x) (int)(x.size())
+#define len(x) (int)(x.length())
+#define whole(x) x.begin(), x.end()
+#define FOR(i, x, y) for(int i=x; i<=y; ++i)
+#define FORl(i, x, y) for(int i=x; i<y; ++i)
+#define FORb(i, x, y) for(int i=x; i>=y; --i)
+#define FORlb(i, x, y) for(int i=x; i>y; --i)
+#define MEMS(x, val) memset(x, val, sizeof(x))
+#define FILEOP() {freopen(task".inp", "r", stdin); }
+#define FILEOP_DEBUG() {freopen(task".inp", "r", stdin); freopen(task".out", "w", stdout); freopen(task".err", "w", stderr);}
+
+
+//=====================================
+//Typedefs
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vb> vvb;
+typedef vector<vii> vvii;
+ordered_set magic;
+char type;
+int q, x;
+
+//=====================================
+//Functions and procedures
+//Initialization and preparation
+void FileInit()
+{
+    FILEOP()
+}
+void FileDebug()
+{
+    FILEOP_DEBUG()
+}
+void FileClose()
+{
+    fclose(stdin);
+    fclose(stdout);
+}
+
+//Main Procedure
+int main()
+{
+    //FILEOP();
+    scanf("%d", &q);
+    while(q--)
+    {
+        cin >> type;
+        if(type == 'I')
+        {
+            scanf("%d", &x);
+            magic.insert(x);
+        }
+        if(type == 'D')
+        {
+            scanf("%d", &x);
+            magic.erase(x);
+        }
+        if(type == 'K')
+        {
+            scanf("%d", &x);
+            if(x > (int)magic.size()) printf("invalid\n");
+            else printf("%d\n", *magic.find_by_order(x-1));
+        }
+        if(type == 'C')
+        {
+            scanf("%d", &x);
+            printf("%d\n", magic.order_of_key(x));
+        }
+    }
+    return 0;
+}
